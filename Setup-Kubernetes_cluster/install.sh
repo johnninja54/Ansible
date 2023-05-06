42af7e55e273ca2f0fd5d5530b1bfccabcbdc52b
johnninja54 <131328514+johnninja54@users.noreply.github.com>
2023-05-06 20:10:23 +0700

    Update install.sh

--- Setup-Kubernetes_cluster/install.sh
+++ Setup-Kubernetes_cluster/install.sh
@@ -3,6 +3,12 @@ controlip=$(hostname -I)
 pod_cidr="192.168.0.0/16"
 service_cidr="172.17.1.0/18"
 user="tringuyen"
+Node01="192.168.9.57"
+
+cat <<EOF | tee /etc/ansible/hosts
+[Nodes]
+Node01 ansible_host=$Node01 ansible_port=22 ansible_user=$user
+EOF
 
 cat <<EOF | tee /etc/modules-load.d/modules.conf
 overlay
