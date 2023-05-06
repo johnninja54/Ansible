722ac4e547001982dce7ff41d91f4dd826ac2ad3
johnninja54 <131328514+johnninja54@users.noreply.github.com>
2023-05-06 12:40:51 +0700

    update

--- Setup-Kubernetes_cluster/script/option1.sh
+++ Setup-Kubernetes_cluster/script/option1.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-swapoff -a
-sed -i '/ swap / s/^(.*)$/#1/g' /etc/fstab #for fstab /etc/fstab
+
+cmd=$(swapoff -a)
+cmd=$(sed -i '/ swap / s/^(.*)$/#1/g' /etc/fstab)
+
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
+
 mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+cmd=$(curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg)
+cmd=$(echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list)
 
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s \
-https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+cmd=$(curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl")
 
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+cmd=$(chmod +x ./kubectl)
+cmd=$(mv ./kubectl /usr/local/bin/kubectl)
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 systemctl enable kubelet
 
 #join cluster
-bash /home/script/join.sh
+cmd=$(bash /home/script/join.sh)
--- Setup-Kubernetes_cluster/script/setup.sh
+++ Setup-Kubernetes_cluster/script/setup.sh
@@ -2,46 +2,40 @@
 hostname=$(hostname -s)
 user=$(whoami)
 
-cat <<EOF | sudo tee /etc/modules-load.d/modules.conf
+cat <<- _EOF_ | sudo tee /etc/modules-load.d/modules.conf
 overlay
 br_netfilter
-EOF
+_EOF_
 
 modprobe overlay
 modprobe br_netfilter
 
-cat <<EOF | tee /etc/sysctl.d/k8s.conf
+cat <<- _EOF_ | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
-EOF
+_EOF_
 
 sysctl --system
 
 apt-get update
-apt-get install \
-ca-certificates \
-curl \
-gnupg \
-lsb-release -y
+apt-get install ca-certificates curl gnupg lsb-release -y
 
 mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+cmd=$(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg)
+mkdir -m 0755 -p /etc/apt/sources.list.d
+cmd=$(echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null)
 
 apt-get update
 apt-get install containerd.io
-sed -i 's/disabled_plugins/#disabled_plugins/g' /etc/containerd/config.toml
+cmd=$(sed -i 's/disabled_plugins/#disabled_plugins/g' /etc/containerd/config.toml)
 
 #edit containerd configuration
-cat <<EOF | tee /etc/containerd/config.toml
+cat <<- _EOF_ | tee /etc/containerd/config.toml
 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
     SystemdCgroup = true
-EOF
+_EOF_
 
 systemctl restart containerd
 systemctl enable containerd
