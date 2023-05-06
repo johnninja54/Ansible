4feee2abd8a7a4865e5758251114ebcefd9c7514
johnninja54 <131328514+johnninja54@users.noreply.github.com>
2023-05-06 18:56:46 +0700

    update

--- Setup-Kubernetes_cluster/script/setup.sh
+++ Setup-Kubernetes_cluster/script/setup.sh
@@ -1,42 +1,23 @@
 #!/bin/bash
 hostname=$(hostname -s)
 user=$(whoami)
-
-cat <<- 'EOF' | sudo tee /etc/modules-load.d/modules.conf
-overlay
-br_netfilter
-EOF
-
+cmd=$(cp -rf /home/module/modules.conf /etc/modules-load.d/modules.conf)
+cmd=$(chmod 777 /etc/modules-load.d/modules.conf)
 cmd=$(modprobe overlay)
 cmd=$(modprobe br_netfilter)
-
-cat << 'EOF' | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
-
+cmd=$(cp -rf /home/module/k8s.conf /etc/sysctl.d/k8s.conf)
+cmd=$(chmod 411 /etc/sysctl.d/k8s.conf)
 cmd=$(sysctl --system)
-
 cmd=$(apt-get update)
 cmd=$(apt-get install -y ca-certificates curl gnupg lsb-release)
-
 cmd=$(mkdir -m 0755 -p /etc/apt/keyrings)
 cmd=$(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg)
 cmd=$(mkdir -m 0755 -p /etc/apt/sources.list.d)
 cmd=$(echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null)
-
 cmd=$(apt-get update)
 cmd=$(apt-get install -y containerd.io)
-cmd=$(sed -i 's/disabled_plugins/#disabled_plugins/g' /etc/containerd/config.toml)
-
-#edit containerd configuration
-cat <<- 'EOF' | tee /etc/containerd/config.toml
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-    SystemdCgroup = true
-EOF
-
+cmd=$(cp -rf /home/module/config.toml /etc/containerd/config.toml)
+cmd=$(chmod 411 /etc/containerd/config.toml)
 cmd=$(systemctl restart containerd)
 cmd=$(systemctl enable containerd)
 
