7d4812f23ef13a7cdbc4276cfb97cd1d89fa8e17
johnninja54 <131328514+johnninja54@users.noreply.github.com>
2023-05-07 07:42:00 +0700

    Update install.sh

--- Setup-Kubernetes_cluster/install.sh
+++ Setup-Kubernetes_cluster/install.sh
@@ -24,6 +24,9 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 EOF
 
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 sysctl --system
 
 apt-get update
